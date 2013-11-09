8f2d1a3a366dd56c5f129e6efdd82aa987e707ea
omar9112 <omar91@Christians-MacBook-Pro-2.local>
2013-11-09 17:43:27 -0400

    phoneNumber

--- product.js
+++ product.js
@@ -1,10 +1,9 @@
 module.exports =  { 
-	Product : function (name, model, brand, category, condition, priceMethod, price, description){
+	Product : function (name, model, brand, condition, priceMethod, price, description){
 		this.id = "";
 		this.name = name;
 		this.model = model;
 		this.brand = brand;
-		this.category = category;
 		this.condition = condition;
 		this.priceMethod = priceMethod;
 		this.price = price;
--- server.js
+++ server.js
@@ -210,7 +210,7 @@ app.get('/ProjectServer/currentUser', function(req, res) {
 	var client = new pg.Client(conString);
 	client.connect();
 
-	var query = client.query("SELECT * FROM customer NATURAL JOIN mailingaddress WHERE uid = $1", [currentUser.uid]);
+	var query = client.query("SELECT * FROM customer NATURAL JOIN mailingaddress NATURAL JOIN phonenumber WHERE uid = $1", [currentUser.uid]);
 	
 	query.on("row", function (row, result) {
     	result.addRow(row);
