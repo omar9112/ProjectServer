444f89543c8d5160e9f38974808affbbdfb81c0d
omar9112 <omar91@Christians-MacBook-Pro-2.local>
2013-11-12 22:30:43 -0400

    kiwi-s.com

--- server.js
+++ server.js
@@ -146,7 +146,8 @@ app.configure(function() {
 });
 
 app.get('/', ensureAuthenticated, function(req, res){
-  res.redirect('http://kiwi-s.com/ICOM-5016/ProjectClient/index.html#homePage');
+	res.redirect('http://kiwi-s.com/index.html#homePage');
+  // res.redirect('http://127.0.0.1:8020/ICOM-5016/ProjectClient/index.html#homePage');
 });
 
 // app.get('/account', ensureAuthenticated, function(req, res){
@@ -155,7 +156,8 @@ app.get('/', ensureAuthenticated, function(req, res){
 // });
 
 app.get('/login', function(req, res){
-	res.redirect('http://kiwi-s.com/ICOM-5016/ProjectClient/index.html#login');
+	res.redirect('http://kiwi-s.com/index.html#login');
+	// res.redirect('http://127.0.0.1:8020/ICOM-5016/ProjectClient/index.html#login');
 });
 
 // POST /login
@@ -169,8 +171,8 @@ app.post('/login',
   passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
   function(req, res) {
   	res.type('application/json');
-  	res.jsonp("Hola");
-  	res.redirect('http://kiwi-s.com/ICOM-5016/ProjectClient/index.html#homePage');
+  	res.redirect('http://kiwi-s.com/index.html#homePage');
+  	// res.redirect('http://127.0.0.1:8020/ICOM-5016/ProjectClient/index.html#homePage');
   });
   
   
